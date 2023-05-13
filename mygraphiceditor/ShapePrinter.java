e30d5e055074e82c23c3fbf331dcafd68bb0680e
StasMaster <56350576+StasMaster@users.noreply.github.com>
2023-05-13 13:23:30 +0200

    Add files via upload

--- mygraphiceditor/ShapePrinter.java
+++ mygraphiceditor/ShapePrinter.java
@@ -4,12 +4,12 @@ public class ShapePrinter {
         System.out.println("Shape name: " + shape.getName());
     }
     public static void main(String[] args) {
-        Circle circle = new Circle("Круг", 6.5);
-        Square square = new Square("Квадрат", 10.0);
-        Triangle triangle = new Triangle("Треугольник", 8.0, 6.0);
-        Rectangle rectangle = new Rectangle("Прямоугольник", 6.0, 8.0);
-        Trapezoid trapezoid = new Trapezoid("Трапеция", 3.0, 5.0, 4.0);
-        Rhombus rhombus = new Rhombus("Ромб", 3.5, 6.8);
+        Shape circle = new Circle("Круг", 6.5);
+        Shape square = new Square("Квадрат", 10.0);
+        Shape triangle = new Triangle("Треугольник", 8.0, 6.0);
+        Shape rectangle = new Rectangle("Прямоугольник", 6.0, 8.0);
+        Shape trapezoid = new Trapezoid("Трапеция", 3.0, 5.0, 4.0);
+        Shape rhombus = new Rhombus("Ромб", 3.5, 6.8);
 
         ShapePrinter shapePrinter = new ShapePrinter();
 
