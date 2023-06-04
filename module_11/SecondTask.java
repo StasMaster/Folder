2a19526c51bfa8963609b82b021d753250441c3e
StasMaster <56350576+StasMaster@users.noreply.github.com>
2023-06-04 20:27:30 +0200

    Update SecondTask.java

--- module_11/SecondTask.java
+++ module_11/SecondTask.java
@@ -1,28 +1,21 @@
 package module_11;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Collections;
 
 public class SecondTask {
     public static List<String> uppercaseAndSort(List<String> strings) {
-        List<String> uppercaseList = new ArrayList<>();
-
-        for (String str : strings) {
-            uppercaseList.add(str.toUpperCase());
-        }
-
-        Collections.sort(uppercaseList, Collections.reverseOrder());
-
-        return uppercaseList;
+        return strings.stream()
+                .map(String::toUpperCase)
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
         List<String> stringsList = List.of("Ivan", "Maria", "Peter", "Olga", "John", "Nekifor", "Fedot", "Elisey", "Feofan");
         List<String> sortedList = uppercaseAndSort(stringsList);
 
-        for (String str : sortedList) {
-            System.out.println(str);
-        }
+        sortedList.forEach(System.out::println);
     }
 }
